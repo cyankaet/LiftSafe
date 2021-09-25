5a4331f3eb7208836352f83891f66db3d551be70
Tobi Alade <aoa34@cornell.edu>
2021-09-25 11:52:52 -0400

    added some drawer changes

--- lift_safe/lib/sidebar.dart
+++ lift_safe/lib/sidebar.dart
@@ -16,17 +16,22 @@ class _NavBarState extends State<NavBar> {
         child: ListView(
       children: [
         DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
             child: Container(
-                child: Text(
-                  "Settings",
-                  textScaleFactor: 2,
-                ),
+                child: Text("Settings",
+                    textScaleFactor: 2,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    )),
                 alignment: Alignment(0.0, 1.0))),
         ListTile(
+            leading: Icon(Icons.message),
             title: const TextField(
                 decoration: const InputDecoration(
                     hintText: "height"))), // TODO ft/inches input
         ListTile(
+            leading: Icon(Icons.accessibility_outlined),
             title: TextField(
                 decoration: const InputDecoration(hintText: "wingspan"),
                 onSubmitted: (text) {
