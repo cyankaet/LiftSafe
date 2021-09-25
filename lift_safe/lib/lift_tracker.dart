67e0cea7be47c6785f1c2bd81d42ba9b31f06314
Peter Cao <ppc49@cornell.edu>
2021-09-25 11:16:09 -0400

    Try limiting accelerometer values

--- lift_safe/lib/lift_tracker.dart
+++ lift_safe/lib/lift_tracker.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 import 'sidebar.dart';
 
@@ -16,6 +17,7 @@ class _LiftTrackerState extends State<LiftTracker> {
   List<double>? _userAccelerometerValues;
   List<double>? _gyroscopeValues;
   List<double> _userAccelerometerZValues = [];
+  List<double> _times = [];
   Stopwatch _stopwatch = new Stopwatch();
   final _streamSubscriptions = <StreamSubscription<dynamic>>[];
   String buttonText = "Start Recording";
@@ -95,8 +97,9 @@ class _LiftTrackerState extends State<LiftTracker> {
   @override
   void initState() {
     super.initState();
-    _streamSubscriptions
-        .add(userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+    _streamSubscriptions.add(userAccelerometerEvents
+        .audit(Duration(seconds: 1))
+        .listen((UserAccelerometerEvent event) {
       setState(() {
         _userAccelerometerValues = <double>[event.x, event.y, event.z];
         _userAccelerometerZValues.add(_userAccelerometerValues?[2] ?? -20000);
