cca445ac3d8b18914a8c34cf4b9e6cb80816e296
Jerry Wang <yw853@cornell.edu>
2021-09-24 23:10:34 -0400

    got accelerometer to work but not gyroscope

---
+++ lift_safe/lib/lift_tracker.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
+
+class LiftTracker extends StatefulWidget {
+  const LiftTracker({Key? key}) : super(key: key);
+
+  @override
+  _LiftTrackerState createState() => _LiftTrackerState();
+}
+
+class _LiftTrackerState extends State<LiftTracker> {
+  List<double>? _userAccelerometerValues;
+  List<double>? _gyroscopeValues;
+  final _streamSubscriptions = <StreamSubscription<dynamic>>[];
+
+  Widget _buildSuggestions() {
+    final List<String>? userAccelerometer = _userAccelerometerValues
+        ?.map((double v) => v.toStringAsFixed(1))
+        .toList();
+    final List<String>? gyroscope =
+        _gyroscopeValues?.map((double v) => v.toStringAsFixed(1)).toList();
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        final l = gyroscope?.length;
+        if (l == null) {
+          return const Divider();
+        }
+        if (index >= l) {
+          return const Divider();
+        }
+        return _buildRow(gyroscope?[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(String? str) {
+    if (str != null) {
+      return ListTile(
+        title: Text(str),
+      );
+    }
+    return const ListTile(
+      title: Text("Accelerometer values finished."),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _streamSubscriptions
+        .add(userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        _userAccelerometerValues = <double>[event.x, event.y, event.z];
+      });
+    }));
+    _streamSubscriptions.add(
+      gyroscopeEvents.listen(
+        (GyroscopeEvent event) {
+          setState(() {
+            _gyroscopeValues = <double>[event.x, event.y, event.z];
+          });
+        },
+      ),
+    );
+  }
+}
